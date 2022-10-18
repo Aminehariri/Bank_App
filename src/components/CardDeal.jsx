983d9e9f357d389f44b15d436eb6c245d2d2d887
=amine <aminehariri57@gmail.com>
2022-10-18 20:09:14 +0100

    starts the testimonials section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { card } from '../assets'
+import styles ,{layout} from '../style'
+import Button from './Button'
+
+
+
+
 
 export default function CardDeal() {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className='sm:block hidden'/> in few easy stops.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5 text-white `}> Arcu tortor purus in mattis at sed integer  Aliquet quis aliquet eget mauris torAliquet ultrices ac ametau </p>
+        <Button  styles='mt-10'/>
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className='w-[100%] h-[100%]' />
+      </div>
+
+    </section>
   )
 }
