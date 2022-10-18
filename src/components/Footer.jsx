3b18f937832d5a295864af2434f6b859831ad3ab
=amine <aminehariri57@gmail.com>
2022-10-18 23:24:10 +0100

    start the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-
+import styles from '../style'
+import { logo } from '../assets'
+import { footerLinks ,socialMedia} from '../constants'
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexCenter} md:flex-row flex-col mb-8 w-full`}>
+
+        <div className='flex-1 flex flex-col justify-start mr-10'>
+          <img src={logo} alt="hoobank" className='w-[266px] h-[72px] object-contain' />
+          <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payment easy.</p>
+        </div>
+
+        <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
+
+        </div>
+
+      </div>
+    </section>
   )
 }
